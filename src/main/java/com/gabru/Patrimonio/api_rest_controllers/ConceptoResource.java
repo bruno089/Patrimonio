d96ff58dd40a78e0f7041e2cb66b123b3e35e1c5
GHGB2021 <gabrielhgb@gmail.com>
2021-02-20 21:41:10 -0300

    Arreglo de obtener por uid y configuraciÃon de variables de BD.

--- src/main/java/com/gabru/Patrimonio/api_rest_controllers/ConceptoResource.java
+++ src/main/java/com/gabru/Patrimonio/api_rest_controllers/ConceptoResource.java
@@ -3,26 +3,38 @@ package com.gabru.Patrimonio.api_rest_controllers;
 import com.gabru.Patrimonio.business_controllers.ConceptoController;
 import com.gabru.Patrimonio.entities.Concepto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/conceptos")
+@RequestMapping(ConceptoResource.CONCEPTOS_ENDPOINT)
 public class ConceptoResource {
+
+    public static final String CONCEPTOS_ENDPOINT = "/conceptos";
+    public static final String CONCEPTO_ID = "/{id}";
+
     @Autowired
     ConceptoController conceptoController;
 
     @GetMapping
-    public List<Concepto> findAll(){
-        return conceptoController.findAll();
+    public List<Concepto> buscarTodosConOtroNombre(){
+        return conceptoController.buscarTodos();
+    }
+
+    @GetMapping(CONCEPTO_ID)
+    public Optional<Concepto> buscar(@PathVariable int id){
+        return conceptoController.buscar(id);
     }
 
     @PostMapping
     public Concepto agregar(){
-        return conceptoController.save();
+        return conceptoController.guardar();
+    }
+
+    @DeleteMapping
+    public void borrar(){
+        conceptoController.borrar();
     }
 }
--- src/main/java/com/gabru/Patrimonio/business_controllers/ConceptoController.java
+++ src/main/java/com/gabru/Patrimonio/business_controllers/ConceptoController.java
@@ -5,8 +5,8 @@ import com.gabru.Patrimonio.repositories.ConceptoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 
@@ -15,16 +15,34 @@ public class ConceptoController {
     @Autowired
     ConceptoRepository conceptoRepository;
 
-    public List<Concepto> findAll(){
+    public List<Concepto> buscarTodos(){
         List<Concepto> conceptosLis;
         conceptosLis = conceptoRepository.findAll();
+
         return conceptosLis;
     }
 
-    public Concepto save(){
+    public Optional<Concepto> buscar(int id){
+        Optional<Concepto> con = conceptoRepository.findById(id);
+
+        if (con.isPresent()){
+            return con;
+        }
+        else{
+            return null;
+        }
+    }
+
+    public Concepto guardar(){
         Concepto concepto = new Concepto();
         concepto.setNombre("Rodo");
         concepto.setIngreso(true);
+
         return conceptoRepository.save(concepto);
+    }
+
+    public void borrar(){
+        int n = 1;
+        conceptoRepository.deleteById(n);
     }
 }
