9f27e0d53a75fce28a386af1aaf3732c26771d60
Bruno Lopez <bruno@ahoradoctor.com>
2022-11-07 21:39:45 -0300

    config Auth Basic Swagger Implementacion

--- src/main/java/com/gabru/Patrimonio/api_rest_controllers/ConceptoRest.java
+++ src/main/java/com/gabru/Patrimonio/api_rest_controllers/ConceptoRest.java
@@ -3,45 +3,42 @@ package com.gabru.Patrimonio.api_rest_controllers;
 import com.gabru.Patrimonio.business_controllers.ConceptoController;
 import com.gabru.Patrimonio.dtos.ConceptoDto;
 import com.gabru.Patrimonio.entities.Concepto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping(ConceptoRest.CONCEPTOS_ENDPOINT)
+@SecurityRequirement(name = "javainuseapi")
 public class ConceptoRest {
     public static final String CONCEPTOS_ENDPOINT = "/conceptos";
     public static final String CONCEPTO_ID = "/{id}";
-
     @Autowired ConceptoController conceptoController;
 
     @GetMapping
+    @Operation(summary = "buscarTodos", security = @SecurityRequirement(name = "basicAuth"))
     public List<Concepto> buscarTodos(){
         return conceptoController.buscarTodos();
     }
-
     @GetMapping("nombre/{nombre}")
     public List<ConceptoDto> buscarTodos(@PathVariable String nombre){
         return conceptoController.buscarPorNombre(nombre);
     }
-
     @GetMapping(CONCEPTO_ID)
     public ConceptoDto buscar(@PathVariable int id){
         return conceptoController.buscar(id);
     }
-
     @PostMapping
     public ConceptoDto agregar(@Valid @RequestBody ConceptoDto conceptoDto){
         return conceptoController.agregar(conceptoDto);
     }
-
     @DeleteMapping(CONCEPTO_ID)
     public void borrar(@PathVariable int id){
         conceptoController.borrar(id);
     } //Todo no permite borrar si ya tiene conceptos asociados¿?
-
-    //todo Bru: Buscar por Filtros
 }
--- src/main/java/com/gabru/Patrimonio/config/SwaggerConfig.java
+++ src/main/java/com/gabru/Patrimonio/config/SwaggerConfig.java
@@ -1,22 +1,76 @@
 package com.gabru.Patrimonio.config;
 
+import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
+@Data
 public class SwaggerConfig {
+
+    private String title;
+    private String description;
+    private String version;
+    private String termsOfServiceUrl;
+    private String license;
+    private String licenseUrl;
+    private String contactName;
+    private String contactUrl;
+    private String contactEmail;
     @Bean
     Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securitySchemes(basicScheme())
+                .securityContexts(Arrays.asList(securityContext()))
+                .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation((RestController.class)))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                ;
+    }
+    private ApiInfo apiInfo(){
+        title = "API Patrimonio";
+        description = "Permite guardar y gestionar gastos e ingresos y claisifcarlos en conceptos." +
+                "Ademas posteriomente consultarlos a traves de algunas consultas establecidas. ";
+        version = "V 1.0.0";
+        termsOfServiceUrl = "Patrimonio API";
+        contactName = "GHGB - Bruno Lopez";
+        contactUrl = "www.brunolopezcross.com";
+        contactEmail = "bruno.lopezcross@gmail.com";
+        license = "Apache 2.0";
+        licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0";
+
+        return new ApiInfo(title, description,version,"",
+                new Contact(contactName,contactUrl,contactEmail)
+                ,license,licenseUrl, Collections.emptyList());
+    }
+    private List<SecurityScheme> basicScheme() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        schemeList.add(new BasicAuth("basicAuth"));
+        return schemeList;
+    }
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global","AccesEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("basicAuth",authorizationScopes));
     }
 }
